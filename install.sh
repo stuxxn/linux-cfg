e86efa90f75cb2fe57dc153e4a17ad960c049c62
stuxxn <stuxxn@gmail.com>
2019-12-03 06:15:59 +0100

    Add install script and tmux.conf

---
+++ install.sh
@@ -0,0 +1,10 @@
+curl -sSL -o ./oh-install.sh https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+echo "Donwloading oh-my-zsh installer..."
+chmod u+x ./oh-install.sh
+echo "Installing oh-my-zsh ..."
+./oh-install.sh
+rm ./oh-install.sh
+
+echo "Creating links to config files"
+ln ./tmux.conf $(HOME)/.tmux.conf
+
